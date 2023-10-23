3f964a6a532503e3110223b4f86e8ce778d2a2fc
nancyrahhal <nancyrahhal97@gmail.com>
2023-10-24 01:28:54 +0300

    Express App setip

---
+++ server.js
@@ -0,0 +1,26 @@
+//imports
+require('dotenv').config()
+const express =require ('express')
+
+
+//express app
+const app = express()
+
+//middleware
+app.use((req,res,next)=>{
+    console.log(req.path, req.method)
+    next()
+})
+
+
+//trying get request
+app.get('/',(req,res)=>{
+    res.json({msg:'Welcome to the LebnenEle'})
+})
+
+
+//listen for requests
+app.listen(process.env.PORT,()=>{
+    console.log('listening on port', process.env.PORT)
+})
+
