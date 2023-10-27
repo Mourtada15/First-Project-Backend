57771188e3950b09791b5c89789ad0c08ef6ddc1
jihad-A-G <jihadabdlghani73@gmail.com>
2023-10-27 12:11:04 +0300

    Added subscriber and lebneneEle tables, multer configured

--- controllers/subscriberControllers.js
+++ controllers/subscriberControllers.js
@@ -1,12 +1,12 @@
 import Subscriber from "../models/subscriberModel.js";
 
-export const addSubscriber = (req,res,next) =>{
+export const addSubscriber = async(req,res,next) =>{
    const {email} =req.body;
    try{
     const sub = new Subscriber({
         email:email
     });
-    sub.save();
+    await sub.save();
     res.status(200).json({message:"Subscrbied successfully!"})
 
 
@@ -17,10 +17,10 @@ export const addSubscriber = (req,res,next) =>{
    }
 }
 
-export const deleteSubscriber =(req,res,next)=>{
+export const deleteSubscriber = async(req,res,next)=>{
     const {email}=req.body;
     try{
-        Subscriber.findOneAndDelete({email:email});
+        await Subscriber.findOneAndDelete({email:email});
     res.status(200).json({message:"Subscrber deleted successfully!"})        
 
     }catch(err){
--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 import  express  from 'express'
 import mongoose from 'mongoose'
 import bodyParser from "body-parser"
+import multer from 'multer'
 import dotenv from "dotenv"
+import path from "path"
 dotenv.config();
 
 
@@ -12,13 +14,41 @@ import aboutUsRoute from "./routes/aboutus.js"
 import articleRoute from "./routes/article.js"
 import contactusRoute from "./routes/contactus.js"
 import milestoneRoute from "./routes/milestone.js"
+import subscriberRoute from './routes/subscriber.js'
+import lebEleRoute from './routes/lebneneEle.js'
 
 //express app
 const app = express()
 
+//Specify where to store the app images (the images folder), and how the naming should be.
+const storage = multer.diskStorage({
+    destination: (req,file,cb)=>{
+        cb(null,'images');
+    },
+    filename:(req,file,cb) =>{
+        cb(null, `${file.fieldname}-${Date.now()}`);
+    }
+});
+
+//Types of file that multer could accept and parse.
+const fileFilter = (req, file, cb) => {
+    if (
+      file.mimetype === 'image/png' ||
+      file.mimetype === 'image/jpg' ||
+      file.mimetype === 'image/jpeg'
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  };
+
+
 //middleware
 app.use(bodyParser.urlencoded({extended:false}))//parse form data
 app.use(express.json())//parsing json data
+app.use(multer({storage:storage,fileFilter:fileFilter}).single('image'));//parsing files
+app.use(express.static('images'));//specify where express should looks for static files
 
 app.use((req,res,next)=>{
     console.log(req.path, req.method)
@@ -31,6 +61,10 @@ app.use('/api/article',articleRoute)
 app.use('/api/about-us', aboutUsRoute)
 app.use('/api/milestone', milestoneRoute)
 app.use('/api/contact-us', contactusRoute)
+app.use('/api/news', subscriberRoute);
+app.use('/api/lebneneEle', lebEleRoute);
+
+
 
 
 //connect to db
