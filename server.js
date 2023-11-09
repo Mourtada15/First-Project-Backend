a51a0fa49ff33831c25e319007c82908779b4c33
jihad-A-G <jihadabdlghani73@gmail.com>
2023-11-09 10:30:55 +0200

    U

--- server.js
+++ server.js
@@ -68,6 +68,7 @@ app.use(async (req,res,next)=>{
 })
 app.use((req,res,next)=>{
     res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods','*');
   next();
   })
 //routes
